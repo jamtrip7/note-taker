ee00a2d07348a8a85c889ab3691e207c90abc81a
jamtrip7 <73972652+jamtrip7@users.noreply.github.com>
2021-04-20 15:49:53 -0400

    updated commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 
 // setting express server
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 // middleware to parse data
 app.use(express.urlencoded({ extended: true }));
